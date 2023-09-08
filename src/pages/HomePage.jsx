4562636becba70c3a40e847a9fb886acfa788bb2
fabianemcortez <fabianemcortez@gmail.com>
2023-09-08 19:41:16 -0300

    Finalizei a home não logada

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,32 +19,27 @@ export default function HomePage() {
           <img src={ImageKids} />
         </div>
 
-
-{/* CARDS */}
+        {/* CARDS */}
         <div className="w-1/2 p-10 flex flex-col justify-center items-center space-y-4">
-
-          <div className="border-2 p-2 rounded-lg text-white w-80" >
+          <div className="border-2 p-2 rounded-lg text-white w-80">
             <h1 className="font-bold text-2xl">Cardápio</h1>
             <p>Desfrute de um acompanhamento diário do cardápio escolar.</p>
           </div>
 
-          <div className="border-2 p-2 rounded-lg text-white w-80" >
+          <div className="border-2 p-2 rounded-lg text-white w-80">
             <h1 className="font-bold text-2xl">Frequência</h1>
             <p>Mantenha o controle da frequência escolar do seu filho.</p>
           </div>
 
-          <div className="border-2 p-2 rounded-lg text-white w-80" >
+          <div className="border-2 p-2 rounded-lg text-white w-80">
             <h1 className="font-bold text-2xl">Notificações</h1>
             <p>Receba todas as notificações em tempo real.</p>
           </div>
 
-          <div className="border-2 p-2 rounded-lg text-white w-80" >
+          <div className="border-2 p-2 rounded-lg text-white w-80">
             <h1 className="font-bold text-2xl">Boletim</h1>
             <p>Esteja ciente das notas e performances</p>
           </div>
-
-
-
         </div>
       </div>
     </div>
