cbb9f7a14c7c36d17cbc573cc71a0e9c98810943
giuprofilo <giulliavoloton@gmail.com>
2023-09-12 01:15:29 -0300

    form ok

--- src/pages/SchoolStudentPage.jsx
+++ src/pages/SchoolStudentPage.jsx
@@ -4,53 +4,58 @@ import Flor1 from "../assets/flor1.svg";
 import Flor2 from "../assets/flor2.svg";
 import Voltar from "../assets/voltar.svg";
 import NavbarSchool from "../components/NavbarSchool";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 
 export default function SchoolStudentPage() {
   // state para armazenar a lista de alunos
   const [users, setUsers] = useState([]);
-  const [formData, setFormData] = useState({
+  const [formStudent, setFormStudent] = useState({
     name: "",
-    password: "",
+    parentsName: "",
     email: "",
+    class: "",
+    register: "",
+    password: "",
   });
+  const [reload, setReload] = useState(false);
 
   useEffect(() => {
     async function getStudents() {
-      const response = await api.get("/user/get_all");
-      setUsers(response.data);
+      try {
+        const response = await api.get("/user/get_all");
+        setUsers(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
     getStudents();
-  }, []);
+  }, [reload]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  function handleChangeStudent(e) {
+    setFormStudent({ ...formStudent, [e.target.name]: e.target.value });
+  }
 
-  const handleSubmit = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
-
     try {
-      const response = await api.post("/user/signup", formData);
-      alert("Aluno cadastrado com sucesso!");
+      const response = await api.post("/user/signup", formStudent);
+      setReload(!reload);
+      toast.success("Aluno criado com sucesso!");
 
-      // Limpar o formulário após o sucesso
-      setFormData({
-        nome: "",
-        password: "",
+      // Limpar os campos do formulário após a adição
+      setFormStudent({
+        name: "",
+        parentsName: "",
         email: "",
+        class: "",
+        register: "",
+        password: "",
       });
-
-      // Você pode fazer algo com a resposta do servidor, se necessário
-      console.log("Resposta do servidor:", response.data);
     } catch (error) {
-      console.error("Erro ao cadastrar aluno:", error);
+      console.log(error);
     }
-  };
+  }
 
   // Função para agrupar os alunos por ano
   const groupUsersByYear = (users) => {
@@ -86,28 +91,27 @@ export default function SchoolStudentPage() {
             </label>
             <input
               type="text"
-              id="name"
-              name="nome"
-              value={formData.nome}
-              onChange={handleInputChange}
+              name="name"
+              value={formStudent.name}
+              onChange={handleChangeStudent}
               className="w-full p-2 border rounded"
               required
             />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="password" className="block text-[#6A7AF5]">
-              Password
+            <label htmlFor="parentsName" className="block text-[#6A7AF5]">
+              Nome dos pais
             </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
+            <textarea
+              type="text"
+              name="parentsName"
+              value={formStudent.parentsName}
+              onChange={handleChangeStudent}
               className="w-full p-2 border rounded"
-              required
             />
           </div>
+
           <div className="mb-4">
             <label htmlFor="email" className="block text-[#6A7AF5]">
               Email
@@ -116,11 +120,59 @@ export default function SchoolStudentPage() {
               type="email"
               id="email"
               name="email"
-              value={formData.email}
-              onChange={handleInputChange}
+              value={formStudent.email}
+              onChange={handleChangeStudent}
               className="w-full p-2 border rounded"
               required
             />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="class" className="block text-[#6A7AF5]">
+              Turma
+            </label>
+            <input
+              type="text"
+              name="class"
+              value={formStudent.class}
+              onChange={handleChangeStudent}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="register" className="block text-[#6A7AF5]">
+              Número de matrícula
+            </label>
+            <input
+              type="text"
+              name="register"
+              value={formStudent.register}
+              onChange={handleChangeStudent}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-[#6A7AF5]">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formStudent.password}
+              onChange={handleChangeStudent}
+              className="w-full p-2 border rounded"
+              required
+            />
+            <button
+              type="submit"
+              className="bg-blue-800 text-white border p-3 mt-5 rounded-lg"
+            >
+              Salvar
+            </button>
           </div>
 
           {/* LISTA DE ALUNOS AGRUPADA POR CLASSE (ANO) */}
