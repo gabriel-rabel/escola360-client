a48f2be84ba139f0fd228b7e82b148aad46157cf
Gabriel Rabel <gabrielrabel@hotmail.com>
2023-09-12 10:30:44 -0300

    PAGINA DE CARDAPIO corrigida

--- src/pages/SchoolGradePage.jsx
+++ src/pages/SchoolGradePage.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import NavbarSchool from "../components/NavbarSchool";
 import Voltar from "../assets/voltar.svg";
-import SubjectEditPage from "./SchoolSubjectEditPage";
 
 export default function SchoolGradePage() {
   const [subjects, setSubjects] = useState([]);
